3c4b11a3777c6070a96fae7d0d921940ed208bf0
Operations Research Engineering Software+ <alex@oresoftware.com>
2019-11-05 00:26:45 -0800

    tmp

--- src/binary-heap.ts
+++ src/binary-heap.ts
@@ -1,3 +1,6 @@
+import * as util from "util";
+import * as treeify from 'treeify';
+
 class Node {
   
   parent: Node;
@@ -6,8 +9,14 @@ class Node {
   value: number;
   
   constructor(value: number, left?: Node, right?: Node) {
-    this.left = left;
-    this.right = right;
+    this.parent = null;
+    this.value = value;
+    this.left = left || null;
+    this.right = right || null;
+  }
+  
+  getValue() {
+    return this.value;
   }
   
   swapWithLeftChild() {
@@ -42,7 +51,9 @@ class Node {
     
     this.parent.left = left;
     this.parent.right = right;
+    const parent = this.parent;
     this.parent = this.parent.parent;
+    parent.parent = this;
     
   }
   
@@ -51,13 +62,41 @@ class Node {
 export class HeapQueue {
   
   root = new Node(1);
+  // foot = this.root;
   
   peek() {
     return this.root;
   }
   
   enq(n: Node) {
-  
+    
+    let root = this.root;
+    
+    while (root) {
+      if (!(root.left && root.right)) {
+        break;
+      }
+      root = root.left;
+    }
+    
+    if (root.left) {
+      root.right = n;
+    }
+    else {
+      root.left = n;
+    }
+    
+    console.log({root});
+    n.parent = root;
+    
+    while (n.parent && n.getValue() < n.parent.getValue()) {
+      n.swapWithParent();
+    }
+    
+    if (!n.parent) {
+      this.root = n;
+    }
+    
   }
   
   deq(n: Node) {
@@ -65,3 +104,25 @@ export class HeapQueue {
   }
   
 }
+
+const h = new HeapQueue();
+h.enq(new Node(2));
+h.enq(new Node(3));
+h.enq(new Node(4));
+h.enq(new Node(5));
+h.enq(new Node(6));
+h.enq(new Node(7));
+h.enq(new Node(8));
+h.enq(new Node(9));
+h.enq(new Node(10));
+h.enq(new Node(11));
+h.enq(new Node(12));
+h.enq(new Node(13));
+h.enq(new Node(14));
+
+
+
+console.log(util.inspect(h, {depth:55, colors: true}));
+
+
+
---
+++ src/breadth-first.ts
@@ -0,0 +1,59 @@
+class Node {
+  nodes: Node[];
+  val: number;
+  
+  constructor(val: number, ...nodes: Node[]) {
+    this.val = val;
+    this.nodes = nodes;
+  }
+  
+}
+
+const n = new Node(
+  1,
+  new Node(2.1),
+  new Node(2.2),
+  new Node(
+    2.3,
+    new Node(4),
+    new Node(
+      5,
+      new Node(6.1),
+      new Node(6.2),
+      new Node(
+        6.3,
+        new Node(7.1),
+        new Node(
+          7.2,
+          new Node(8)
+        )
+      ),
+      new Node(6.4),
+    )
+  ),
+  new Node(2.4),
+);
+
+
+const run = () => {
+  
+  const q = [n];
+  
+  while(q.length > 0){
+  
+    console.log('len:', q.length);
+    
+    const v = q.pop();
+    
+    console.log(v.val);
+    
+    for(let n of v.nodes){
+      q.unshift(n);
+    }
+  }
+  
+};
+
+run();
+
+
